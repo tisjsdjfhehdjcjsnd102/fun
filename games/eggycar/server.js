c5a185a6a3149a1a629132a693215e425ed29600
gelly5082 <gekhoury@wis.edu>
2026-05-09 21:38:32 -0400

    Add files via upload

---
+++ games/eggycar/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the root directory
+app.use(express.static(__dirname));
+
+// Handle all routes by serving index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+}); 
