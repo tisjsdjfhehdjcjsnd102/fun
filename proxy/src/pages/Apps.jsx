78c20f6ad1c4fdfb2aa845e240b6dde3925437e9
tisjsdjfhehdjcjsnd102 <gekhoury@wis.edu>
2026-05-12 16:30:50 -0400

    Delete proxy directory

--- proxy/src/pages/Apps.jsx
+++
@@ -1,7 +0,0 @@
-import AppLayout from '../layouts/Apps';
-import { memo } from 'react';
-
-const Apps = memo(() => <AppLayout />);
-
-Apps.displayName = 'Apps';
-export default Apps;
--- proxy/src/pages/Apps2.jsx
+++
@@ -1,7 +0,0 @@
-import GamesLayout from '../layouts/Apps2';
-import { memo } from 'react';
-
-const Gms = memo(() => <GamesLayout />);
-
-Gms.displayName = 'Games';
-export default Gms;
